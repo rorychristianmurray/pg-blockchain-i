33ee6738be9c42d5af00b92e874c8425ae370ecb
Rory Murray <rory.christian.murray@gmail.com>
2019-10-03 11:43:30 -0400

    Create addBlock method

--- blockchain.js
+++ blockchain.js
@@ -4,4 +4,11 @@ class Blockchain {
   constructor() {
     this.chain = [Block.genesis()];
   }
+
+  addBlock(data) {
+    const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+    this.chain.push(block);
+
+    return block;
+  }
 }
