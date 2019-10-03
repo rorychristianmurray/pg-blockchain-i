190ca31ab7e25a91b7d60b4fa81514b838b41600
Rory Murray <rory.christian.murray@gmail.com>
2019-10-03 10:32:24 -0400

    Consolde log block

--- dev-test.js
+++ dev-test.js
@@ -1,1 +1,8 @@
 const Block = require("./block");
+
+const genesis = Block.genesis();
+const block = Block.mineBlock(genesis, "foo");
+
+console.log("dev-test genesis: ", genesis.toString());
+console.log("dev-test block: ", block.toString());
+// const block = Block.mineBlock(genesis, "foo");
